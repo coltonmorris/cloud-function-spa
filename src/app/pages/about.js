0f51e8d1c60651e7009662874dbf499a21be0a9a
colton <colton.morris@tcnbroadcasting.com>
2018-03-05 10:28:29 -0700

    serving images

--- src/app/pages/about.js
+++ src/app/pages/about.js
@@ -1,8 +1,15 @@
 import React from "react"
 import App from "../components/App"
 
+
 export default () => (
   <App>
-    <p>About Page</p>
+    <p>My name is Colton, and I am a software developer at TCN, inc.</p>
+    <a href="https://github.com/coltonmorris">
+      <img style={{"background":"red"}} src='/static/GitHub-Mark-64px.png' alt="1" height="64" width="64"/>
+    </a>
+    <a href="https://www.linkedin/in/coltonmorris">
+      <img style={{"background":"blue"}} src='/static/linkedin.ico' alt="3" height="64" width="64"/>
+    </a>
   </App>
 )
--- src/app/pages/index.js
+++ src/app/pages/index.js
@@ -1,8 +1,12 @@
 import React from "react"
 import App from "../components/App"
 
+// import '../static/node_modules/font-awesome/css/font-awesome.min.css' 
+import 'font-awesome/css/font-awesome.min.css'
+
 export default () => (
   <App>
     <p>Index Page</p>
+    <i className="fas fa-camera-retro"></i>
   </App>
 )
