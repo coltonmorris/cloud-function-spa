e6965d71100a79c3c8816b341ae847a077c2b63d
colton <colton.morris@tcnbroadcasting.com>
2018-03-17 16:43:36 -0600

    added postcss, fontawesome, and appropriate loaders

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,13 +1,26 @@
 import React from "react"
 import Link from "next/link"
+import _ from "lodash"
+
+import stylesheet from '../static/font-awesome/web-fonts-with-css/css/fontawesome.css'
+import fontawesome from '../static/font-awesome/svg-with-js/js/fontawesome-all'
+
+let style = {
+}
 
 export default ({ pathname }) => (
   <header>
+    <style dangerouslySetInnerHTML={{ __html: fontawesome }} />
+    <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
+
     <Link href="/">
       <a className={pathname === "/" ? "is-active" : ""}>Home</a>
     </Link>{" "}
     <Link href="/about">
       <a className={pathname === "/about" ? "is-active" : ""}>About</a>
     </Link>
+    <a style={_.assign({}, style, {"color":"#000"})} className="fa-stack fa-1x" data-toggle="tooltip" title="Github" data-placement="top" href="https://github.com/coltonmorris">
+      <i className="fab fa-github-square fa-stack-2x"></i>
+    </a>
   </header>
 )
